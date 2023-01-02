c567afae10c6b167e13d3532187dc7044a9f6c0d
goldenhearten7 <66818449+taruntailor7@users.noreply.github.com>
2023-01-03 01:31:09 +0530

    Completed

--- models/users.model.js
+++ models/users.model.js
@@ -4,10 +4,6 @@ const userSchema = new mongoose.Schema({
     username: {
         type: String,
         required: true,
-        validate: {
-            validator: /^[0-9a-zA-Z]+$/, 
-            message: "Please enter alphanumeric username only!"
-        },
     },
 },{timestamps: true, versionKey: false});
 
