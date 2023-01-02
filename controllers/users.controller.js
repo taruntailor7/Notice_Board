4e1a7965277b642c7545ce89bfad93fe2bef1ec8
goldenhearten7 <66818449+taruntailor7@users.noreply.github.com>
2023-01-03 01:24:19 +0530

    Completed

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -4,15 +4,28 @@ import { userModel } from "../models/users.model.js";
 export const login = async (req, res) => {
     try {
         let username = req.body;
-        let newUser = await userModel.create(username);
-        newUser = newUser.toJSON();
-        return res.send({
-            error:false,
-            message: 'User successfully logged in!',
-            data: newUser
-        })
+
+        const regEx = /^[0-9a-zA-Z]+$/;
+
+        if(username.match(regEx)){
+            let newUser = await userModel.create({username});
+            newUser = newUser.toJSON();
+            return res.send({
+                error:false,
+                message: 'User successfully logged in!',
+                data: newUser
+            })
+        } else{
+            return res.send({
+                error:true,
+                message: 'Please enter alphanumeric username only!'
+            })
+        }
     } catch (error) {
-        return res.status(500).send("Please anter alphanunmric username only!");
+        return res.status(500).send({
+            message: "Something went wrong!"
+        });
     }
 }
+
 
