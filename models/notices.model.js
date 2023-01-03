d707a5865e3d405b3ca616c97e0db3b37e9611d1
goldenhearten7 <66818449+taruntailor7@users.noreply.github.com>
2023-01-03 18:07:07 +0530

    Completed

--- models/notices.model.js
+++ models/notices.model.js
@@ -9,10 +9,6 @@ const noticeSchema = new mongoose.Schema({
     username:{
         type: String,
         required: true,
-    },
-    date:{
-        type: String,
-        required: true,
     }
 },{timestamps: true, versionKey: false})
 
